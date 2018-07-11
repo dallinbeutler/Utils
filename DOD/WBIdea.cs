8e4092e2ddd1b5f0ae1d2f516c709105865fe4f3
Dallin Beutler <BeutlerD@corp.lcl>
2018-07-11 16:37:10 -0600

    World Building thought layout. Not really related, coulda been a gist

---
+++ DOD/WBIdea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DOD
+{
+   class Entity
+   {
+      int ID;
+      List<Snapshot> states;
+   }
+   class Snapshot
+   {
+      string Name;
+      string ShortDescription;
+      string longDescription;
+      List<string> tags;
+      List<Link> links;
+      string picUrl;
+   }
+   class Link
+   {
+      Entity link1;
+      Entity link2;
+
+      string description;
+   }
+}
